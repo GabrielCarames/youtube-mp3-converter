8a69d105f769618cc3ccae4ba7a498c25b962818
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-11-24 13:56:26 -0300

    feat: add header

---
+++ components/Header.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image"
+import logo from "../public/logo.webp"
+import githubIcon from "../public/github-icon.png"
+
+export default function Header() {
+  return (
+    <>
+      <div className="mx-auto max-w-7xl px-4">
+        <div className="relative flex h-16 items-center justify-between pt-5">
+          <div className="flex flex-1 items-center justify-between">
+            <div className="flex items-center gap-2">
+              <div className="flex justify-center items-center bg-[#086788] rounded-lg w-10">
+                <div>
+                  <Image
+                    className="h-8 w-auto"
+                    src={logo}
+                    alt="Your asd"
+                    style={{ objectFit: "cover" }}
+                  />
+                </div>
+              </div>
+              <h3>Free File Converter</h3>
+            </div>
+            <div className="flex items-center gap-2">
+              <Image className="" src={githubIcon} alt="Logo de Github" width={30} height={30} />
+              <a
+                href="https://github.com/GabrielCarames/free-file-converter"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Repositiorio
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
