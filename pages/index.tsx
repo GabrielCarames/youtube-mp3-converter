939917a1f8dfa90f12cc5181bae96b957f3f7f7d
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-11-24 14:08:39 -0300

    feat: add default styles to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import PageLayout from "../components/PageLayout"
-import styles from "../styles/Home.module.css"
 
 export default function Home() {
   return (
     <PageLayout>
-      <div className={styles.container}></div>
+      <div className="w-auto h-full flex justify-center items-center bg-red-500">
+        {/* <div className="w-full h-96 ">
+          <h1>Free File Converter</h1>
+          <p>Convierte cualquier archivo a otro formato</p>
+        </div> */}
+      </div>
     </PageLayout>
   )
 }
