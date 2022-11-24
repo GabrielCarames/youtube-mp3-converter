c2124396544497decc9fc06c148970b59a703375
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-11-24 16:15:32 -0300

    change: change color pelette

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,13 +8,23 @@ export default function Header() {
       <div className="relative flex h-16 items-center justify-center pt-5">
         <div className="w-full max-w-[1200px] flex flex-1 items-center justify-between">
           <div className="flex items-center gap-2">
-            <div className="flex justify-center items-center bg-[#07A0C3] rounded-lg w-10">
+            <div className="flex justify-center items-center bg-[#8D72E1] rounded-lg w-10">
               <Image className="h-8 w-auto" src={logo} alt="Logo de Free File Converter" />
             </div>
             <h3>Free File Converter</h3>
           </div>
           <div className="flex items-center gap-2">
-            <Image className="" src={githubIcon} alt="Logo de Github" width={30} height={30} />
+            <Image
+              className=""
+              src={githubIcon}
+              alt="Logo de Github"
+              width={30}
+              height={30}
+              style={{
+                filter:
+                  "invert(31%) sepia(14%) saturate(3812%) hue-rotate(224deg) brightness(101%) contrast(92%)"
+              }}
+            />
             <a
               href="https://github.com/GabrielCarames/free-file-converter"
               target="_blank"
