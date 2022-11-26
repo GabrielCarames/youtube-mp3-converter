f7efb14e368f39144a993a60b16b72bd8d467d5f
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-11-26 14:53:05 -0300

    feat: add useConversions hook

---
+++ components/Conversions/useConversions.ts
@@ -0,0 +1,11 @@
+export const useConversions = () => {
+  const formatDuration = (duration: number) =>
+    new Date(duration * 1000).toISOString().substring(14, 19)
+
+  const getVideoThumbnail = (link: string) => {
+    const videoId = link.match(/id=(.*?)&/)![1]
+    return videoId ? `https://img.youtube.com/vi/${videoId}/0.jpg` : ""
+  }
+
+  return { formatDuration, getVideoThumbnail }
+}
