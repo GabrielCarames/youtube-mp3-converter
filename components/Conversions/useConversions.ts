66450ddc45ec43757f4db127247b7b24a816007f
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-11-26 19:15:47 -0300

    fix: fix thumbnail not showing

--- components/Conversions/useConversions.ts
+++ components/Conversions/useConversions.ts
@@ -3,8 +3,10 @@ export const useConversions = () => {
     new Date(duration * 1000).toISOString().substring(14, 19)
 
   const getVideoThumbnail = (link: string) => {
-    const videoId = link.match(/id=(.*?)&/) && [1]
-    return videoId ? `https://img.youtube.com/vi/${videoId}/0.jpg` : ""
+    if (link) {
+      const videoId = link.match(/id=(.*?)&/)![1]
+      return `https://img.youtube.com/vi/${videoId}/0.jpg`
+    } else return ""
   }
 
   return { formatDuration, getVideoThumbnail }
