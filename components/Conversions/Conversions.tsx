7ce8696d4ddb8968237f272a3bda355b9bb6d502
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-11-26 15:07:09 -0300

    feat: add conversion list

--- components/Conversions/Conversions.tsx
+++ components/Conversions/Conversions.tsx
@@ -1,18 +1,57 @@
+import Image from "next/image"
 import { conversionProps } from "../../interfaces"
+import { useConversions } from "./useConversions"
+import downloadIcon from "../../public/download-icon.svg"
 
 export default function Conversions({ conversions }: { conversions: conversionProps[] }) {
+  const { formatDuration, getVideoThumbnail } = useConversions()
+
   return (
-    <div>
-      <h1>Conversions</h1>
-      <ul>
-        {conversions.map((conversion, index) => {
-          return (
-            <li key={index}>
-              <h2>{conversion.title}</h2>
-            </li>
-          )
-        })}
-      </ul>
+    <div className="w-full max-w-[1200px] flex flex-col items-center ">
+      <div className="w-full bg-[#7C7A7A] flex flex-col items-center px-4 py-4 box-border rounded-3xl">
+        <h1 className="text-[22px] text-white leading-[50px]">Conversiones</h1>
+        <ul className="w-full flex flex-wrap justify-between gap-4">
+          {conversions.map((conversion, index) => {
+            return (
+              <li
+                className="w-full max-w-[570px] flex justify-center gap-8 bg-[#494949] p-5 px-2 box-border text-white rounded-3xl border-dashed border-[#7C7A7A] border-4"
+                key={index}
+              >
+                <header className="flex justify-centera items-center gap-5">
+                  <Image
+                    className=""
+                    src={getVideoThumbnail(conversion.link)}
+                    alt="Miniatura del video"
+                    width={60}
+                    height={60}
+                  />
+                  <div>
+                    <h2>{conversion.title}</h2>
+                    <p>{formatDuration(conversion.duration)}</p>
+                  </div>
+                </header>
+                <a
+                  className="w-full max-w-[200px] p-5 box-border rounded-lg bg-[#FF5D73] font-bold flex items-center gap-4"
+                  href=""
+                  download={conversion.link}
+                >
+                  Descargar MP3
+                  <Image
+                    src={downloadIcon}
+                    alt="Descargar MP3"
+                    width={20}
+                    height={20}
+                    style={{
+                      filter:
+                        " invert(99%) sepia(1%) saturate(0%) hue-rotate(32deg) brightness(114%) contrast(100%)"
+                    }}
+                  />
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
   return (
     <PageLayout>
-      <div className="w-auto h-full px-5 box-border flex flex-col justify-center items-center">
+      <div className="w-auto h-full px-5 box-border flex flex-col justify-center items-center gap-10">
         <div className="w-full flex flex-col gap-10 items-center">
           <div className="w-full h-auto flex flex-col items-center">
             <h1 className="text-[32px] font-bold">Youtube MP3 Converter</h1>
