ddad912d54a37e9d3bb7eb73ceb2aa639ff82aef
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-11-26 18:59:25 -0300

    fix: fix video link was null

--- components/Conversions/useConversions.ts
+++ components/Conversions/useConversions.ts
@@ -3,7 +3,7 @@ export const useConversions = () => {
     new Date(duration * 1000).toISOString().substring(14, 19)
 
   const getVideoThumbnail = (link: string) => {
-    const videoId = link.match(/id=(.*?)&/)![1]
+    const videoId = link.match(/id=(.*?)&/) && [1]
     return videoId ? `https://img.youtube.com/vi/${videoId}/0.jpg` : ""
   }
 
